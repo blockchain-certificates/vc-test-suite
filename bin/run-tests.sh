dd84695a928b606f3796bc4313060d79191f484b
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 14:46:01 +0100

    chore(DevX): debug bin command

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env
 
+echo "starting issuer..."
 npm run start:issuer
+echo "issuer started, running tests"
 npm run test
+echo "tests have been run, stopping issuer"
 npm run stop:issuer
+echo "vc test suite compliance run finished"
