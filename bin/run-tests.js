66c1415f1abb09e2a8a2751b381f37bb6637edaa
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 15:34:48 +0100

    chore(DevX): fix cwd to detect command

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -5,14 +5,14 @@ const path = require('path');
 
 console.log('starting issuer...');
 execSync('npm run start:issuer', {
-  cwd: path.join(__dirname, '../../..')
+  cwd: path.join(__dirname, '../')
 });
 console.log('issuer started, running tests');
 execSync('npm run test', {
-  cwd: path.join(__dirname, '../../..')
+  cwd: path.join(__dirname, '../')
 });
 console.log('tests have been run, stopping issuer');
 execSync('npm run stop:issuer', {
-  cwd: path.join(__dirname, '../../..')
+  cwd: path.join(__dirname, '../')
 });
 console.log('vc test suite compliance run finished');
