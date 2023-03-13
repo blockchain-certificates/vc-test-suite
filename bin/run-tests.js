8498b08e62e48a3ceeeabf480b819e5a6777c08a
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 15:13:22 +0100

    chore(DevX): run npm commands from js file

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 
+const { exec } = require('child_process');
+
 console.log("starting issuer...");
-// npm run start:issuer
+exec("npm run start:issuer");
 console.log("issuer started, running tests");
-// npm run test
+exec("npm run test");
 console.log("tests have been run, stopping issuer");
-// npm run stop:issuer
+exec("npm run stop:issuer");
 console.log("vc test suite compliance run finished");
