801a9e20b8ca4acd818ee4f22cbc591a695d24d5
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 15:41:04 +0100

    chore(DevX): attempt running bin with shelljs

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -2,17 +2,23 @@
 
 const { execSync } = require('child_process');
 const path = require('path');
+const shelljs = require('shelljs');
 
 console.log('starting issuer...');
-execSync('npm run start:issuer', {
-  cwd: path.join(__dirname, '../')
-});
+shelljs.exec('cd ..');
+shelljs.exec('npm run start:issuer');
+
+// execSync('npm run start:issuer', {
+//   cwd: path.join(__dirname, '../')
+// });
 console.log('issuer started, running tests');
-execSync('npm run test', {
-  cwd: path.join(__dirname, '../')
-});
+shelljs.exec('npm run test');
+// execSync('npm run test', {
+//   cwd: path.join(__dirname, '../')
+// });
 console.log('tests have been run, stopping issuer');
-execSync('npm run stop:issuer', {
-  cwd: path.join(__dirname, '../')
-});
+shelljs.exec('npm run stop:issuer');
+// execSync('npm run stop:issuer', {
+//   cwd: path.join(__dirname, '../')
+// });
 console.log('vc test suite compliance run finished');
