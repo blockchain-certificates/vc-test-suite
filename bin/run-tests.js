b99e73334ca281e08f809e791d191b5c548b41a9
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 16:23:26 +0100

    chore(DevX): circumvent mocha not found error

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -12,13 +12,11 @@ spawn('npm', ['run start:issuer'], {
 
 console.log('cwd', process.cwd());
 console.log('issuer started, running tests');
-shelljs.exec('npm run test');
-// execSync('npm run test', {
-//   cwd: path.join(__dirname, '../')
-// });
+execSync('npm run test', {
+  cwd: path.join(__dirname, '../')
+});
 console.log('tests have been run, stopping issuer');
-shelljs.exec('npm run stop:issuer');
-// execSync('npm run stop:issuer', {
-//   cwd: path.join(__dirname, '../')
-// });
+execSync('npm run stop:issuer', {
+  cwd: path.join(__dirname, '../')
+});
 console.log('vc test suite compliance run finished');
