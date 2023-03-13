e5a52c9c3b6043722dbe72a3e92b500cf79f82fb
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 17:07:05 +0100

    chore(DevX): fix cwd

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -1,9 +1,12 @@
 #!/usr/bin/env node
 
 const { exec } = require('child_process');
+const path = require('path');
 
 console.log('cwd', process.cwd());
-exec(`sh ${__dirname}/run-tests.sh`, (error, stdout, stderr) => {
+exec(`sh ${__dirname}/run-tests.sh`, {
+  cwd: path.join(__dirname, '..')
+}, (error, stdout, stderr) => {
   if (error) {
     console.error(error);
   }
