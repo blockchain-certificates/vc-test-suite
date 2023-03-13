b6d485a0bc57cc08d6780fcfbe119926d81a5c13
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 15:10:20 +0100

    chore(DevX): attempt with js format

---
+++ bin/run-tests.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+console.log("starting issuer...");
+// npm run start:issuer
+console.log("issuer started, running tests");
+// npm run test
+console.log("tests have been run, stopping issuer");
+// npm run stop:issuer
+console.log("vc test suite compliance run finished");
