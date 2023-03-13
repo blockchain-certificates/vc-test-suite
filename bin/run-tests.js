3bbc08bf797ac5953e884f21094d279654e54b19
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-13 15:22:21 +0100

    chore(DevX): import path

--- bin/run-tests.js
+++ bin/run-tests.js
@@ -1,17 +1,18 @@
 #!/usr/bin/env node
 
 const { execSync } = require('child_process');
+const path = require('path');
 
-console.log("starting issuer...");
-execSync("npm run start:issuer", {
+console.log('starting issuer...');
+execSync('npm run start:issuer', {
   cwd: path.join(__dirname, '../../..')
 });
-console.log("issuer started, running tests");
-execSync("npm run test", {
+console.log('issuer started, running tests');
+execSync('npm run test', {
   cwd: path.join(__dirname, '../../..')
 });
-console.log("tests have been run, stopping issuer");
-execSync("npm run stop:issuer", {
+console.log('tests have been run, stopping issuer');
+execSync('npm run stop:issuer', {
   cwd: path.join(__dirname, '../../..')
 });
-console.log("vc test suite compliance run finished");
+console.log('vc test suite compliance run finished');
