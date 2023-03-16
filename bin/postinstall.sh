06a1419c15f54c97e4a79f2cacf0b2b48b47d063
Julien Fraichot <julien.fraichot@hyland.com>
2023-03-16 17:02:27 +0100

    chore(DevX): debug

--- bin/postinstall.sh
+++ bin/postinstall.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env
 
+echo "vc-test-suite postinstall script"
+pwd 
 mkdir node_modules/vc-test-suite/node_modules
 cp -r node_modules/cert-issuer-node-wrapper node_modules/vc-test-suite/node_modules/cert-issuer-node-wrapper
