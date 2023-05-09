7b430dda70d0268b10974f30d17cfad8437ddd1d
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-09 16:55:24 +0200

    fix(TesT): run with npx

--- bin/run-blockcerts-report.sh
+++ bin/run-blockcerts-report.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env
 
 pwd
-echo "installing mocha globally"
-npm i -g mocha mocha-spec-json-output-reporter chai
 echo "starting issuer..."
 npm run start:issuer
 echo "issuer started, running blockcerts reporting tests"
@@ -13,7 +11,7 @@ do
   REPORT_NAME=${ENTRY%.js*}
   REPORT_NAME=${REPORT_NAME##*/}
   echo $REPORT_NAME
-  mocha "$ENTRY" -R mocha-spec-json-output-reporter --reporter-options fileName=report-${REPORT_NAME}.json,hierarchy=true
+  npx mocha "$ENTRY" -R mocha-spec-json-output-reporter --reporter-options fileName=report-${REPORT_NAME}.json,hierarchy=true
 done
 echo "tests have been run, stopping issuer"
 npm run stop:issuer
