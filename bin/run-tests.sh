ab415fc2489b2576e3ffbd522a96494e91729210
Julien Fraichot <julien.fraichot@hyland.com>
2023-05-09 14:45:40 +0200

    fix(Tests): run tests sequentially to prevent early failure of async tests

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env
 
 pwd
+echo "installing mocha globally"
+npm i -g mocha
 echo "starting issuer..."
 npm run start:issuer
 echo "issuer started, running tests"
-npm run test
+tests_dir=./test/vc-data-model-1.0
+
+for entry in "$tests_dir"/*.js
+do
+  mocha "$entry"
+done
 echo "tests have been run, stopping issuer"
 npm run stop:issuer
 echo "vc test suite compliance run finished"
